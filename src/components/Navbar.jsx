88f18783e5f595642703cc27fe08ad0eafb5c655
KaleAtharva <atharvaakale@gmail.com>
2024-09-25 16:13:02 +0530

    added _redirects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import "./css/Navbar.css";
 
 const Navbar = () => {
   const [scrolled, setIsScrolled] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false); // State to handle mobile menu toggle
 
   React.useEffect(() => {
     const handleScroll = () => {
@@ -22,33 +23,49 @@ const Navbar = () => {
     };
   }, []);
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen); // Toggle the menu open/close
+  };
+
   return (
     <header id="header" className={scrolled ? "header-scrolled" : ""}>
       <div className="containers">
         <div id="logo" className="pull-left">
           <img src="img/ACES Logo White Transparent.png" id="img" alt="" />
         </div>
-        <nav id="nav-menu-container">
-          <ul className="nav-menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/team">Team</Link>
-            </li>
-            <li>
-              <Link to="/events">Events</Link>
-            </li>
-            <li>
-              <Link to="/newsletter">NewsLetter</Link>
-            </li>
-            <li>
-              <Link to="/magazine">Magazine</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
 
+        <nav id="nav-menu-container">
+          <ul className={`nav-menu ${isOpen ? "open" : ""}`}>
+            <li>
+              <Link to="/" onClick={() => setIsOpen(false)}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/team" onClick={() => setIsOpen(false)}>
+                Team
+              </Link>
+            </li>
+            <li>
+              <Link to="/events" onClick={() => setIsOpen(false)}>
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link to="/newsletter" onClick={() => setIsOpen(false)}>
+                NewsLetter
+              </Link>
+            </li>
+            <li>
+              <Link to="/magazine" onClick={() => setIsOpen(false)}>
+                Magazine
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={() => setIsOpen(false)}>
+                Contact
+              </Link>
+            </li>
             <li className="buy-tickets">
               <a
                 href="https://forms.gle/vTMqz9FQrSYNAaVG7"
@@ -60,6 +77,11 @@ const Navbar = () => {
             </li>
           </ul>
         </nav>
+
+        {/* Hamburger icon */}
+        <div className="mobile-menu-icon" onClick={handleToggle}>
+          <span className={isOpen ? "hamburger open" : "hamburger"}></span>
+        </div>
       </div>
     </header>
   );
