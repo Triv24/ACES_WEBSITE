80b4f8db9c5f34c7b062a74195708b86189e4fa4
KaleAtharva <atharvaakale@gmail.com>
2024-09-25 15:21:01 +0530

    intro changes

--- src/components/Home_Components/Intro.jsx
+++ src/components/Home_Components/Intro.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Intro = () => {
   return (
     <section id="intro">
-    <div className="intro-container">
-      <h1 className="mb-4 pb-0">PVG’s College of Engineering and Technology & G. K. Pate (Wani) Institute of Management</h1>
-      <p className="mb-4 pb-0">Association Of Computer Engineering Students</p><h1><span>(ACES)</span></h1>
-      <a href="https://www.youtube.com/watch?v=tbyuHRS17HQ" className="venobox play-btn mb-4" data-vbtype="video"
-        data-autoplay="true"></a>
-    </div>
-  </section>
-  )
-}
+      <div className="intro-container">
+        <h1 className="mb-4 pb-0">
+          PVG’s College of Engineering and Technology & G. K. Pate (Wani)
+          Institute of Management
+        </h1>
+        <p className="mb-4 pb-0">
+          Association Of Computer Engineering Students
+        </p>
+        <h1>
+          <span>(ACES)</span>
+        </h1>
+        <a
+          href="https://www.youtube.com/watch?v=tbyuHRS17HQ"
+          className="venobox play-btn mb-4"
+          data-vbtype="video"
+          data-autoplay="true"
+          rel="noreferrer"
+          target="_blank"
+        >
+          .
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
